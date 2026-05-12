07c1c296128aeda9ccfdca05a2f2a3bc4124c049
aordaz3 <alejandr.r.ordaz@gmail.com>
2026-05-12 13:39:45 -0600

    added gson mapper

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -5,9 +5,9 @@ import handler.UserHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import io.javalin.json.JsonMapper;
-import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
+
 
 public class Server {
 
@@ -15,9 +15,25 @@ public class Server {
     private final UserHandler userHandler = new UserHandler();
 
     public Server() {
-        Gson gson = new GsonBuilder().create();
 
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        Gson gson = new GsonBuilder().create();
+        JsonMapper gsonMapper = new JsonMapper() {
+            @Override
+            public String toJsonString(Object obj, Type type) {
+                return gson.toJson(obj, type);
+            }
+
+            @Override
+            public <T> T fromJsonString(String json, Type targetType) {
+                return gson.fromJson(json, targetType);
+            }
+        };
+
+        javalin = Javalin.create(config -> {
+            config.jsonMapper(gsonMapper);
+            config.staticFiles.add("web");
+
+        });
 
         javalin.post("/user", ctx -> userHandler.register(ctx));
         javalin.post("/session", ctx -> userHandler.login(ctx));
