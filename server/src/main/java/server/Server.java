7173924527edff196cb44757ff434729699eaf5a
aordaz3 <alejandr.r.ordaz@gmail.com>
2026-05-12 13:30:22 -0600

    transtion to gson

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,6 +2,12 @@ package server;
 
 import io.javalin.Javalin;
 import handler.UserHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.javalin.json.JsonMapper;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Type;
 
 public class Server {
 
@@ -9,7 +15,10 @@ public class Server {
     private final UserHandler userHandler = new UserHandler();
 
     public Server() {
+        Gson gson = new GsonBuilder().create();
+
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+
         javalin.post("/user", ctx -> userHandler.register(ctx));
         javalin.post("/session", ctx -> userHandler.login(ctx));
         javalin.delete("/session", ctx -> userHandler.logout(ctx));
