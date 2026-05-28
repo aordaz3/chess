6b985284103342c0cdb7f8d5e805cbb5d917231b
aordaz3 <alejandr.r.ordaz@gmail.com>
2026-05-28 09:52:02 -0600

    set up for each test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,15 +3,26 @@ package client;
 import org.junit.jupiter.api.*;
 import server.Server;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static int port;
+    private static String serverUrl;
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        port = server.run(0);
+        serverUrl = "http://localhost:" + port;
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -19,8 +30,17 @@ public class ServerFacadeTests {
     static void stopServer() {
         server.stop();
     }
+    @BeforeEach
+    public void clearDatabase() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + "/db"))
+                .DELETE()
+                .build();
 
-
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertEquals(200, response.statusCode());
+        facade = new ServerFacade(serverUrl);
+    }
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
