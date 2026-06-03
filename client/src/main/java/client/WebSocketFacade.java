eaf834cb57cbfb8de9381b9255e3a8af699d4216
aordaz3 <alejandro.r.ordaz@gmail.com>
2026-06-03 12:10:23 -0600

    leave

--- client/src/main/java/client/WebSocketFacade.java
+++ client/src/main/java/client/WebSocketFacade.java
@@ -2,6 +2,7 @@ package client;
 
 import chess.ChessMove;
 import com.google.gson.Gson;
+import websocket.commands.UserGameCommand;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -13,6 +14,8 @@ public class WebSocketFacade {
 
 
     private final String websocketUrl;
+    private final Gson gson = new Gson();
+    private WebSocket webSocket;
 
     public WebSocketFacade(String websocketUrl) {
         this.websocketUrl = websocketUrl;
@@ -26,6 +29,9 @@ public class WebSocketFacade {
     }
 
     public void leave(String authToken, Integer gameID) {
+        UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID);
+        sendRaw(command);
+        close();
     }
 
     public void resign(String authToken, Integer gameID) {
@@ -33,6 +39,23 @@ public class WebSocketFacade {
     }
 
     public void close() {
+        try {
+            if (webSocket != null) {
+                webSocket.sendClose(WebSocket.NORMAL_CLOSURE, "bye").join();
+            }
+        }
+        catch (Exception e) {
+            System.out.println("couldnt leave");
+        }
+
+    }
+
+    private void sendRaw(Object command) {
+        if (webSocket == null) {
+            throw new IllegalStateException("WebSocket is not connected.");
+        }
+        String json = gson.toJson(command);
+        webSocket.sendText(json, true).join();
     }
 
 }
